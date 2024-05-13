af62b07f44068dfcb7b2f8aa528851b41dca3236
jassir <jassir1902@gmail.com>
2024-05-12 21:09:45 -0400

    Clase PatientCodeGenerator creada

---
+++ Practice3Library/Managers/PatientCodeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPB.Practice3Library.Managers
+{
+    public class PatientCodeGenerator
+    {
+        public string GeneratePatientCode(string name, string lastName, string ci)
+        {
+            // Tomamos las iniciales del nombre y apellido y las convertimos a mayúsculas
+            string initials = $"{name.Substring(0, 1)}{lastName.Substring(0, 1)}".ToUpper();
+
+            // Generamos el código del paciente concatenando las iniciales y el número de CI
+            string patientCode = $"{initials}-{ci}";
+
+            return patientCode;
+        }
+    }
+}
