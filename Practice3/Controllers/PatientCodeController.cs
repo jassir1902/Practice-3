6a4a1d60f2fe9a6c7e19fd9756e318f201c5fcaf
jassir <jassir1902@gmail.com>
2024-05-12 21:32:22 -0400

    PatientsCodeController configurado

---
+++ Practice3/Controllers/PatientCodeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UPB.Practice3Library.Managers;
+using UPB.Practice3Library.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UPB.Practice3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientCodeController : ControllerBase
+    {
+        private readonly PatientCodeGenerator _patientCodeGenerator;
+
+        public PatientCodeController()
+        {
+            _patientCodeGenerator = new PatientCodeGenerator();
+        }
+
+        [HttpPost]
+        public ActionResult<string> GeneratePatientCode([FromBody] PatientData patientData)
+        {
+            // Verifica si los datos del paciente son válidos
+            if (string.IsNullOrEmpty(patientData.Name) || string.IsNullOrEmpty(patientData.LastName) || string.IsNullOrEmpty(patientData.CI))
+            {
+                return BadRequest("Nombre, Apellido y CI son requeridos.");
+            }
+
+            // Genera el código del paciente utilizando la lógica de la biblioteca de clases
+            var patientCode = _patientCodeGenerator.GeneratePatientCode(patientData.Name, patientData.LastName, patientData.CI);
+            return Ok(patientCode);
+        }
+    }
+}
---
+++ Practice3Library/Models/PatientData.cs
@@ -0,0 +1,9 @@
+﻿namespace UPB.Practice3Library.Models
+{
+    public class PatientData
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CI { get; set; }
+    }
+}
